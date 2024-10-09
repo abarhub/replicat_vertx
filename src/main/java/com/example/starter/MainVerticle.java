c3e8dd6fb24f897bb1fb4d78843f11f06fb513e7
abarhub <abarhub@gmail.com>
2024-10-09 21:37:30 +0200

    debut implémentation

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,17 +1,47 @@
 package com.example.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private AtomicInteger counter = new AtomicInteger(1);
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onComplete(http -> {
+
+    HttpServer server = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+
+//    router.route().handler(ctx -> {
+//
+//      // This handler will be called for every request
+//      HttpServerResponse response = ctx.response();
+//      response.putHeader("content-type", "text/plain");
+//
+//      // Write to the response and end it
+//      response.end("Hello World from Vert.x-Web!");
+//    });
+
+    router
+      .post("/init")
+      // this handler will ensure that the response is serialized to json
+      // the content type is set to "application/json"
+      .respond(
+        ctx -> {
+          int nb=counter.getAndIncrement();
+          return Future.succeededFuture(nb);
+        });
+
+    server.requestHandler(router).listen(8080)
+      .onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
@@ -19,5 +49,18 @@ public class MainVerticle extends AbstractVerticle {
         startPromise.fail(http.cause());
       }
     });
+
+//    vertx.createHttpServer().requestHandler(req -> {
+//      req.response()
+//        .putHeader("content-type", "text/plain")
+//        .end("Hello from Vert.x!");
+//    }).listen(8888).onComplete(http -> {
+//      if (http.succeeded()) {
+//        startPromise.complete();
+//        System.out.println("HTTP server started on port 8888");
+//      } else {
+//        startPromise.fail(http.cause());
+//      }
+//    });
   }
 }
