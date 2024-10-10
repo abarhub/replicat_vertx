c6f3e872e3fec86944209f7da10c5732c240d351
abarhub <abarhub@gmail.com>
2024-10-10 08:22:31 +0200

    implémentation des urls

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -1,20 +1,31 @@
 package com.example.starter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainVerticle extends AbstractVerticle {
+
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   private AtomicInteger counter = new AtomicInteger(1);
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
+
+    int port = 7070;
 
     HttpServer server = vertx.createHttpServer();
 
@@ -30,25 +41,72 @@ public class MainVerticle extends AbstractVerticle {
 //      response.end("Hello World from Vert.x-Web!");
 //    });
 
-    router
-      .post("/init")
-      // this handler will ensure that the response is serialized to json
-      // the content type is set to "application/json"
-      .respond(
-        ctx -> {
-          int nb=counter.getAndIncrement();
-          return Future.succeededFuture(nb);
-        });
+    router.route("/init").method(HttpMethod.POST).blockingHandler(
+      ctx -> {
+        int nb = counter.getAndIncrement();
 
-    server.requestHandler(router).listen(8080)
-      .onComplete(http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
+        HttpServerResponse response = ctx.response();
+        response.putHeader("content-type", "text/plain");
+//      var res=Future.succeededFuture(nb);
+        LOGGER.atInfo().log("init id: {}", nb);
+        response.end("" + nb);
       }
-    });
+    );
+
+
+    router.route("/listeFichiers/:id").method(HttpMethod.POST).blockingHandler(
+      ctx -> {
+//        int nb = counter.getAndIncrement();
+        String id = ctx.pathParam("id");
+        LOGGER.atInfo().log("listeFichiers id: {}", id);
+        HttpServerResponse response = ctx.response();
+        response.putHeader("content-type", "text/plain");
+//      var res=Future.succeededFuture(nb);
+        var tmp = new ListFiles2();
+        tmp.setListe(new ArrayList<>());
+        tmp.setCode("");
+        ObjectMapper objectMapper = new ObjectMapper();
+        var res = "";
+        try {
+          res = objectMapper.writeValueAsString(tmp);
+        } catch (JsonProcessingException e) {
+          LOGGER.atError().log("JsonProcessingException", e);
+        }
+        response.end(res);
+      }
+    );
+
+    router.route("/upload/:id").method(HttpMethod.POST).blockingHandler(
+      ctx -> {
+//        int nb = counter.getAndIncrement();
+        String id = ctx.pathParam("id");
+        LOGGER.atInfo().log("upload id: {}", id);
+        HttpServerResponse response = ctx.response();
+        response.putHeader("content-type", "text/plain");
+//      var res=Future.succeededFuture(nb);
+        response.end("");
+      }
+    );
+
+//    router
+//      .post("/init")
+//      // this handler will ensure that the response is serialized to json
+//      // the content type is set to "application/json"
+//      .respond(
+//        ctx -> {
+//          int nb = counter.getAndIncrement();
+//          return Future.succeededFuture(nb);
+//        });
+
+    server.requestHandler(router).listen(port)
+      .onComplete(http -> {
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("HTTP server started on port " + port);
+        } else {
+          startPromise.fail(http.cause());
+        }
+      });
 
 //    vertx.createHttpServer().requestHandler(req -> {
 //      req.response()
@@ -64,3 +122,25 @@ public class MainVerticle extends AbstractVerticle {
 //    });
   }
 }
+
+class ListFiles2 {
+  private List<String> liste;
+  private String code;
+
+  public List<String> getListe() {
+    return liste;
+  }
+
+  public void setListe(List<String> liste) {
+    this.liste = liste;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+}
+
